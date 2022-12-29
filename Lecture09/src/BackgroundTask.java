c5dd43a7067f184e9b63527f44d5f1707a10c318
Sercan Külcü <sercankulcu@gmail.com>
2022-12-29 23:57:05 +0300

    added new examples

---
+++ Lecture09/src/BackgroundTask.java
@@ -0,0 +1,38 @@
+/*
+ * here is another simple Java program that demonstrates how to use a thread to perform a task 
+ * in the background:
+ * 
+ * his program defines a BackgroundTask class that implements the Runnable interface and has a 
+ * run method. This method simply prints out a message and sleeps for 1 second, 100 times.
+ * */
+
+public class BackgroundTask implements Runnable {
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("Running task: " + i);
+      try {
+        Thread.sleep(1000); // Sleep for 1 second
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  
+  public static void main(String[] args) {
+    BackgroundTask task = new BackgroundTask();
+    Thread thread = new Thread(task);
+    thread.start();
+    
+    // Do some other work here
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Doing other work: " + i);
+      try {
+        Thread.sleep(500); // Sleep for 0.5 seconds
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    
+    System.out.println("Done.");
+  }
+}
