33e2b2e715cafcb6700e2dbbabee2f741fab5867
Sercan Külcü <sercankulcu@gmail.com>
2023-01-03 11:40:51 +0300

    rename

--- Lecture05/src/SortAlphabetically.java
+++ Lecture05/src/SortAlphabetically.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SortalAhabetically {
+public class SortAlphabetically {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
