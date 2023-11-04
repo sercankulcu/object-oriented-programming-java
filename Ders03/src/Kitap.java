1a5b69ea59bd17cc5cb5720f1838b8770bdb495b
Sercan Külcü <sercankulcu@gmail.com>
2023-11-05 02:09:10 +0300

    corrects

--- Ders03/src/Kitap.java
+++ Ders03/src/Kitap.java
@@ -1,20 +1,40 @@
 
 public class Kitap {
-	String ad;
+	String baslik;
 	String yazar;
 	int sayfaSayisi;
 
-	public Kitap(String ad) {
-		this.ad = ad;
+	public Kitap() {
+		
+	}
+	
+	public Kitap(String baslik) {
+		this.baslik = baslik;
 	}
 
-	public Kitap(String ad, String yazar) {
-		this(ad);  // Constructor chaining
+	public Kitap(String baslik, String yazar) {
+		this(baslik);  // Constructor chaining
 		this.yazar = yazar;
 	}
 
-	public Kitap(String ad, String yazar, int sayfaSayisi) {
-		this(ad, yazar);  // Constructor chaining
+	public Kitap(String baslik, String yazar, int sayfaSayisi) {
+		this(baslik, yazar);  // Constructor chaining
 		this.sayfaSayisi = sayfaSayisi;
+	}
+	
+	private void kitapBilgisiGoster() {
+		System.out.println(baslik + " " + yazar + " " + sayfaSayisi);
+	}
+
+	public static void main(String[] args) {
+		//Bir Kitap nesnesi oluşturalım
+		Kitap kitap = new Kitap();
+
+		//Kitap nesnesinin özelliklerine erişim
+		kitap.baslik = "Java Programlama";
+		kitap.yazar = "John Doe";
+
+		//Kitap nesnesinin metotlarına erişim
+		kitap.kitapBilgisiGoster();
 	}
 }
