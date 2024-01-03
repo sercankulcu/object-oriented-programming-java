c7a14cda0d552ca1b4ab7c9ecf8464390b2ce6fb
Sercan Külcü <sercankulcu@gmail.com>
2024-01-03 13:54:01 +0300

    added new examples

---
+++ Ders09/src/CapitalizeClient.java
@@ -0,0 +1,23 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class CapitalizeClient {
+	public static void main(String[] args) throws Exception {
+		/*if (args.length != 1) {
+			System.err.println("Pass the server IP as the sole command line argument");
+			return;
+		}*/
+		try (var socket = new Socket("localhost", 59898)) {
+			System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
+			try (var scanner = new Scanner(System.in)) {
+				var in = new Scanner(socket.getInputStream());
+				var out = new PrintWriter(socket.getOutputStream(), true);
+				while (scanner.hasNextLine()) {
+					out.println(scanner.nextLine());
+					System.out.println(in.nextLine());
+				}
+			}
+		}
+	}
+}
