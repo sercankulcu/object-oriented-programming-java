447f14a67b5f065f3d9958651c4d7356d727dda1
Sercan Külcü <sercankulcu@gmail.com>
2024-12-07 20:44:19 +0300

    Update AralikToplama.java

--- Ders03/src/forloop/AralikToplama.java
+++ Ders03/src/forloop/AralikToplama.java
@@ -2,25 +2,37 @@ package forloop;
 import java.util.Scanner;
 
 public class AralikToplama {
-	
-	public static void aralikTopla(int a, int b) {
-		int toplam = 0;
-		for (int i = a; i <= b; i++) {
-			toplam += i;
-		}
-		System.out.println(a + " ile " + b + " arasındaki sayıların toplamı: " + toplam);		
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Başlangıç değerini girin (a): ");
-		int a = scanner.nextInt();
+    // Verilen araliktaki (a'dan b'ye kadar) sayilarin toplamini hesaplayan metot
+    public static void aralikTopla(int a, int b) {
+        int toplam = 0; // Toplam degiskeni baslatilir
+        for (int i = a; i <= b; i++) { // a'dan b'ye kadar olan sayilar donguyle eklenir
+            toplam += i; // Her sayi toplam degiskenine eklenir
+        }
+        // Toplam sonucu ekrana yazdirilir
+        System.out.println(a + " ile " + b + " arasindaki sayilarin toplami: " + toplam);
+    }
 
-		System.out.print("Bitiş değerini girin (b): ");
-		int b = scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		aralikTopla(a, b);
-		scanner.close();
-	}
+        // Kullanicidan baslangic degeri (a) alinir
+        System.out.print("Baslangic degerini girin (a): ");
+        int a = scanner.nextInt();
+
+        // Kullanicidan bitis degeri (b) alinir
+        System.out.print("Bitis degerini girin (b): ");
+        int b = scanner.nextInt();
+
+        // aralikTopla metodu cagrilarak verilen araliktaki sayilarin toplami hesaplanir
+        if(a < b) {
+        	aralikTopla(a, b);
+        }
+        else {
+        	aralikTopla(b, a);
+        }
+
+        // Scanner kapatilir
+        scanner.close();
+    }
 }
