5422c52ba2f1452e8ae1c2a6348b03ae3dadd593
Sercan Külcü <sercankulcu@gmail.com>
2024-12-10 23:57:24 +0300

    .

--- Ders09/src/date/DateClient.java
+++ Ders09/src/date/DateClient.java
@@ -1,18 +1,15 @@
-package date;
+package date; 
 import java.util.Scanner;
 import java.net.Socket;
 import java.io.IOException;
 
-/**
- * A command line client for the date server. Requires the IP address of the
- * server as the sole argument. Exits after printing the response.
- */
 public class DateClient {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		try (var socket = new Socket("localhost", 59090)) {
-			var in = new Scanner(socket.getInputStream());
-			System.out.println("Server response: " + in.nextLine());
-		}
-	}
+        // Sunucuya baglanmak icin "localhost" adresini ve 59090 portunu kullan
+        try (var socket = new Socket("localhost", 59090)) { 
+            var in = new Scanner(socket.getInputStream()); // Sunucudan gelen veriyi okumak icin Scanner nesnesi
+            System.out.println("Sunucu yaniti: " + in.nextLine()); // Sunucudan gelen mesaji ekrana yazdir
+        }
+    }
 }
--- Ders09/src/date/DateServer.java
+++ Ders09/src/date/DateServer.java
@@ -1,25 +1,22 @@
-package date;
+package date; 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.Date;
 
-/**
- * A simple TCP server. When a client connects, it sends the client the current
- * datetime, then closes the connection. This is arguably the simplest server
- * you can write. Beware though that a client has to be completely served its
- * date before the server will be able to handle another client.
- */
 public class DateServer {
-	public static void main(String[] args) throws IOException {
-		try (var listener = new ServerSocket(59090)) {
-			System.out.println("The date server is running...");
-			while (true) {
-				try (var socket = listener.accept()) {
-					var out = new PrintWriter(socket.getOutputStream(), true);
-					out.println(new Date().toString());
-				}
-			}
-		}
-	}
+	
+    public static void main(String[] args) throws IOException {
+    	
+        try (var listener = new ServerSocket(59090)) { // ServerSocket nesnesi olustur, 59090 portunda dinler
+            System.out.println("Tarih sunucusu calisiyor..."); // Serverin calistigini ekrana yazdir
+            
+            while (true) { // Sonsuz dongu, her yeni baglanti icin islem yapacak
+                try (var socket = listener.accept()) { // Bir client baglantisi kabul et
+                    var out = new PrintWriter(socket.getOutputStream(), true); // Cikti akisini olustur
+                    out.println(new Date().toString()); // Client'a bugunun tarihini gonder
+                }
+            }
+        }
+    }
 }
