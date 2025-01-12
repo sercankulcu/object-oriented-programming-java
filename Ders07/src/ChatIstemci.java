d61c298325444956a8b80944f5f88fcd521842ca
Sercan Külcü <sercankulcu@gmail.com>
2025-01-12 20:20:10 +0300

    .

--- Ders07/src/ChatIstemci.java
+++ Ders07/src/ChatIstemci.java
@@ -10,28 +10,28 @@ import java.util.Scanner;
 public class ChatIstemci {
 
     public static void main(String[] args) throws IOException {
-        // Bir soket olustur
-        Socket soket = new Socket("localhost", 8000);
+        try (// Bir soket olustur
+		Socket soket = new Socket("localhost", 8000)) {
+			// Veri alisverisi icin girdi ve cikti akislari olustur
+			DataInputStream sunucudanGelenVeri = new DataInputStream(soket.getInputStream());
+			DataOutputStream sunucuyaGidenVeri = new DataOutputStream(soket.getOutputStream());
 
-        // Veri alisverisi icin girdi ve cikti akislari olustur
-        DataInputStream sunucudanGelenVeri = new DataInputStream(soket.getInputStream());
-        DataOutputStream sunucuyaGidenVeri = new DataOutputStream(soket.getOutputStream());
+			try (// Konsoldan veri okumak icin bir scanner olustur
+			Scanner scanner = new Scanner(System.in)) {
+				while (true) {
+				    // Konsoldan bir mesaj oku
+				    String mesaj = scanner.nextLine();
 
-        // Konsoldan veri okumak icin bir scanner olustur
-        Scanner scanner = new Scanner(System.in);
+				    // Mesaji sunucuya gonder
+				    sunucuyaGidenVeri.writeUTF(mesaj);
 
-        while (true) {
-            // Konsoldan bir mesaj oku
-            String mesaj = scanner.nextLine();
+				    // Sunucudan gelen mesaji oku
+				    mesaj = sunucudanGelenVeri.readUTF();
 
-            // Mesaji sunucuya gonder
-            sunucuyaGidenVeri.writeUTF(mesaj);
-
-            // Sunucudan gelen mesaji oku
-            mesaj = sunucudanGelenVeri.readUTF();
-
-            // Mesaji ekranda goster
-            System.out.println(mesaj);
-        }
+				    // Mesaji ekranda goster
+				    System.out.println(mesaj);
+				}
+			}
+		}
     }
 }
